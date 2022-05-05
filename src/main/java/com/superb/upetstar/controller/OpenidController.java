7a6cef4f4265b94ab67a01db33610a4a507f30ca
hym <ymworkmail2021@163.com>
2022-05-05 10:06:02 +0800

    feat:新增openid controller

---
+++ src/main/java/com/superb/upetstar/controller/OpenidController.java
@@ -0,0 +1,59 @@
+package com.superb.upetstar.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class OpenidController {
+
+
+    @RequestMapping("/testopenid")
+    public String getUserInfo(@RequestParam(name = "code") String code) throws Exception {
+        System.out.println("code=" + code);
+        String url = "https://api.weixin.qq.com/sns/jscode2session";
+        url += "?appid=wx39b86046fe4bff1b";//自己的appid
+        url += "&secret=1451156e616708ff2d06680273bd247b";//自己的appSecret
+        url += "&js_code=" + code;
+        url += "&grant_type=authorization_code";
+        url += "&connect_redirect=1";
+        String res = null;
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        // DefaultHttpClient();
+        HttpGet httpget = new HttpGet(url);    //GET方式
+        CloseableHttpResponse response = null;
+        // 配置信息
+        RequestConfig requestConfig = RequestConfig.custom()          // 设置连接超时时间(单位毫秒)
+                .setConnectTimeout(5000)                    // 设置请求超时时间(单位毫秒)
+                .setConnectionRequestTimeout(5000)             // socket读写超时时间(单位毫秒)
+                .setSocketTimeout(5000)                    // 设置是否允许重定向(默认为true)
+                .setRedirectsEnabled(false).build();           // 将上面的配置信息 运用到这个Get请求里
+        httpget.setConfig(requestConfig);                         // 由客户端执行(发送)Get请求
+        response = httpClient.execute(httpget);                   // 从响应模型中获取响应实体
+        HttpEntity responseEntity = response.getEntity();
+        System.out.println("响应状态为:" + response.getStatusLine());
+        if (responseEntity != null) {
+            res = EntityUtils.toString(responseEntity);
+            System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+            System.out.println("响应内容为:" + res);
+        }
+        // 释放资源
+        httpClient.close();
+        response.close();
+        JSONObject jo = JSON.parseObject(res);
+        String openid = jo.getString("openid");
+        System.out.println("openid" + openid);
+        return openid;
+    }
+
+}
