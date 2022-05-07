25061e7c5cc9512fcc26e6e220977a0342e7af52
hym <ymworkmail2021@163.com>
2022-05-07 15:11:01 +0800

    build:Maven打包跳过自动测试

--- src/test/java/com/superb/upetstar/TestES.java
+++ src/test/java/com/superb/upetstar/TestES.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 
